13f066f784adae77f17f407a0572dcffdda65cf0
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2021-08-20 00:38:02 -0700

    changed port

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const indexRouter = require('./routes/index');
 const app = express();
 const path = require('path');
 
+const port = process.env.PORT || 3000;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -17,5 +19,5 @@ app.use(express.json());
 
 app.use('/', indexRouter);
 
-app.listen(3000)
-console.log('listening on port 3000')
+app.listen(port)
+console.log(`listening on port ${port}`)
